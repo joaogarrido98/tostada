e3898d73609dc84876c99d5b8508aaa18d8e634c
João Garrido <joao.melo.garrido@hotmail.com>
2021-04-07 13:16:58 +0100

    second commit

--- tostas/src/main/java/com/example/tostas/Tostas.kt
+++ tostas/src/main/java/com/example/tostas/Tostas.kt
@@ -1,26 +1,67 @@
-package com.example.tostas
+package com.example.torrada
 
 import android.content.Context
+import android.os.CountDownTimer
 import android.widget.Toast
 
-object Tostas {
+object Torrada {
     @JvmStatic
-    fun error(context: Context, message: String, time: Int) {
-        Toast.makeText(context, message, time).show()
+    fun error(context: Context, message: String, time: Long) {
+        val toast = Toast.makeText(context, message, Toast.LENGTH_LONG)
+        val toastCountDown: CountDownTimer
+        toastCountDown = object:CountDownTimer(time, 1000) {
+            override fun onTick(millisUntilFinished:Long) {
+               toast.show()
+            }
+            override fun onFinish() {
+                toast.cancel()
+            }
+        }
+        toastCountDown.start()
     }
 
     @JvmStatic
-    fun success(context: Context, message: String, time: Int) {
-        Toast.makeText(context, message , time).show()
+    fun success(context: Context, message: String, time: Long) {
+        val toast = Toast.makeText(context, message, Toast.LENGTH_LONG)
+        val toastCountDown: CountDownTimer
+        toastCountDown = object:CountDownTimer(time, 1000) {
+            override fun onTick(millisUntilFinished:Long) {
+                toast.show()
+            }
+            override fun onFinish() {
+                toast.cancel()
+            }
+        }
+        toastCountDown.start()
     }
 
     @JvmStatic
-    fun info(context: Context, message: String, time: Int) {
-        Toast.makeText(context, message, time).show()
+    fun info(context: Context, message: String, time: Long) {
+        val toast = Toast.makeText(context, message, Toast.LENGTH_LONG)
+        val toastCountDown: CountDownTimer
+        toastCountDown = object:CountDownTimer(time, 1000) {
+            override fun onTick(millisUntilFinished:Long) {
+                toast.show()
+            }
+            override fun onFinish() {
+                toast.cancel()
+            }
+        }
+        toastCountDown.start()
     }
 
     @JvmStatic
-    fun warn(context: Context, message: String, time: Int) {
-        Toast.makeText(context, message, time).show()
+    fun warn(context: Context, message: String, time: Long) {
+        val toast = Toast.makeText(context, message, Toast.LENGTH_LONG)
+        val toastCountDown: CountDownTimer
+        toastCountDown = object:CountDownTimer(time, 1000) {
+            override fun onTick(millisUntilFinished:Long) {
+                toast.show()
+            }
+            override fun onFinish() {
+                toast.cancel()
+            }
+        }
+        toastCountDown.start()
     }
 }
