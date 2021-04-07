8dc8e40d166184ea19e2b4c129f940d8e0c110ad
João Garrido <joao.melo.garrido@hotmail.com>
2021-04-07 15:01:00 +0100

    bigger text size

--- tostas/src/main/java/com/example/tostas/Tostas.kt
+++ tostas/src/main/java/com/example/tostas/Tostas.kt
@@ -1,7 +1,6 @@
 package com.example.tostas
 
 import android.content.Context
-import android.graphics.drawable.Drawable
 import android.view.LayoutInflater
 import android.view.View
 import android.widget.TextView
