a6bed5c892ebd15c9573f499efa681e0b20cd5d7
João Garrido <joao.melo.garrido@hotmail.com>
2021-04-07 16:39:47 +0100

    some optimizations

--- tostas/src/main/java/com/example/tostas/Tostas.kt
+++ tostas/src/main/java/com/example/tostas/Tostas.kt
@@ -15,41 +15,35 @@ object Tostas {
 
     @JvmStatic
     fun error(context: Context, message: String, time: Int) {
-        val view: View = LayoutInflater.from(context).inflate(R.layout.toast_layout_error, null)
-        val toastTextView = view.findViewById(R.id.custom_error) as TextView
-        toastTextView.text = message
-        val toast = Toast.makeText(context, message, time)
-        toast.duration = time
-        toast.view = view
-        toast.show()
+        val layout = R.layout.toast_layout_error
+        val textView = R.id.custom_error
+        inflate(context,layout, textView, message,time)
     }
 
     @JvmStatic
     fun success(context: Context, message: String, time: Int) {
-        val view: View = LayoutInflater.from(context).inflate(R.layout.toast_layout_success, null)
-        val toastTextView = view.findViewById(R.id.custom_success) as TextView
-        toastTextView.text = message
-        val toast = Toast.makeText(context, message, time)
-        toast.duration = time
-        toast.view = view
-        toast.show()
+        val layout = R.layout.toast_layout_success
+        val textView = R.id.custom_success
+        inflate(context,layout, textView, message,time)
     }
 
     @JvmStatic
     fun info(context: Context, message: String, time: Int) {
-        val view: View = LayoutInflater.from(context).inflate(R.layout.toast_layout_info, null)
-        val toastTextView = view.findViewById(R.id.custom_info) as TextView
-        toastTextView.text = message
-        val toast = Toast.makeText(context, message, time)
-        toast.duration = time
-        toast.view = view
-        toast.show()
+        val layout = R.layout.toast_layout_info
+        val textView = R.id.custom_info
+        inflate(context,layout, textView, message,time)
     }
 
     @JvmStatic
     fun warn(context: Context, message: String, time: Int) {
-        val view: View = LayoutInflater.from(context).inflate(R.layout.toast_layout_warn, null)
-        val toastTextView = view.findViewById(R.id.custom_warn) as TextView
+        val layout = R.layout.toast_layout_warn
+        val textView = R.id.custom_warn
+        inflate(context,layout, textView, message,time)
+    }
+
+    fun inflate(context: Context,layout: Int, textView: Int, message:String, time: Int){
+        val view: View = LayoutInflater.from(context).inflate(layout, null)
+        val toastTextView = view.findViewById(textView) as TextView
         toastTextView.text = message
         val toast = Toast.makeText(context, message, time)
         toast.duration = time
